b218f0e5b87c2e106ad8d607b2de5610f50f819a
Thiago Borges <thiagoborges@amhp.com.br>
2021-04-08 09:45:13 -0300

    Navegação pelos consoles

---
+++ src/DP.ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DP.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opcoes();
+        }
+        
+        private static void OpcaoSelecionada()
+        {
+            var operacao = Console.ReadKey().KeyChar.ToString().ToUpper();
+
+            switch (operacao)
+            {
+                case "S":
+                    Console.Clear();                    
+                    break;
+                case "1":
+                    _ = new CreationalPatterns.CreationalPatterns();
+                    Console.Clear();
+                    Opcoes();
+                    break;
+                default:
+                    OperacaoInvalida();
+                    break;
+            }
+        }
+
+        private static void OperacaoInvalida()
+        {
+            Console.Clear();
+            Console.WriteLine("Opção selecionada é inválida!");
+            Console.WriteLine("");
+            Opcoes();            
+        }
+
+        private static void Opcoes()
+        {
+            Console.WriteLine("Selecione o padrão:");
+            Console.WriteLine("");
+            Console.WriteLine("1 - Creational Patterns (Padrões de criação)");
+            Console.WriteLine("S - Sair");
+            OpcaoSelecionada();
+        }
+    }
+}
---
+++ src/DP.CreationalPatterns/CreationalPatterns.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DP.CreationalPatterns
+{
+    public class CreationalPatterns
+    {
+        public CreationalPatterns()
+        {
+            Console.Clear();
+            Opcoes();
+            var opcao = Console.ReadKey().ToString();
+            Executar(opcao);
+        }
+
+        private void Executar(string opcao)
+        {
+            switch (opcao)
+            {
+                case "S":
+                    Console.Clear();
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Opção inválida!");
+                    Console.WriteLine();
+                    Opcoes();
+                    break;
+            }
+        }
+
+        private void Opcoes()
+        {            
+            Console.WriteLine("Selecione uma opção");
+            Console.WriteLine("S - Sair");
+        }
+    }
+}
